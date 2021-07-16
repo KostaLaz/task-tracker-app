69ab305c65604cd05edb4ee6d9de9512f13cde23
KostaLaz <kosta.lazarevski89@gmail.com>
2021-07-17 01:22:54 +0200

    add and display new task

--- src/App.js
+++ src/App.js
@@ -36,10 +36,18 @@ function App() {
       )
   }
 
+  const addTask = (task) => {
+
+    const id = Math.floor(Math.random() * 10000 +1)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+    console.log(id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
      {tasks.length > 0 ? <Tasks  onToggle={toggleReminder} tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show' }
     </div>
   );
