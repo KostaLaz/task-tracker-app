200a753d31b8701cafd6ea446eb9133fe743be0c
KostaLaz <kosta.lazarevski89@gmail.com>
2021-07-16 17:38:20 +0200

    Tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Jul 15th at 14:45',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Car repair',
+        day: 'Jul 19th at 11:15',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Gym training',
+        day: 'Jul 15th at 18:00',
+        reminder: true,
+    },
+]
+
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
